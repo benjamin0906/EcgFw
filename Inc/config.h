d0e1bbaf73df896261c727532ffa6a535a31981f
benjamin0906 <benjamin0906@gmail.com>
2022-06-29 17:23:56 +0200

    ADAS pins have been added

--- Inc/config.h
+++ Inc/config.h
@@ -13,6 +13,9 @@
 #define ADAS_SCLK   PortA_5
 #define ADAS_SDO    PortA_6
 #define ADAS_SDI    PortA_7
+#define ADAS_PD     PortC_4
+#define ADAS_RESET  PortC_5
+#define ADAS_CLKIO  PortB_1
 
 #define SUPPLY_VOLTAGE      33
 #define USB_RX_BUFFER_SIZE  127 //This needs to be less by 1 than a power of 2
